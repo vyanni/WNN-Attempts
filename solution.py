4de824389aeb2da330125ccbd6073a32f5f3bd73
vyanni <vianneysusanto@gmail.com>
2026-01-20 21:49:32 -0500

    Transformer tryout

--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from example_solution.utils import DataPoint
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -22,31 +23,59 @@ class PositionalEncoding:
         return encoding
 
 class AttentionHead(nn.Module):
-    def __init__(self, input_dimensions, num_heads):
+    def __init__(self, input_dimensions):
         super(AttentionHead, self).__init__()
         self.input_dimensions = input_dimensions
-        self.num_heads = num_heads
-    
-    def attentionCalculation(self):
-        query = np.zeros((self.num_heads, self.input_dimensions))
-        key = np.zeros((self.num_heads, self.input_dimensions))
-        value = np.zeros((self.num_heads, self.input_dimensions))
 
-    
+        self.queryWeights = np.random.rand(self.num_heads, self.input_dimensions)
+        self.keyWeights = np.random.rand(self.num_heads, self.input_dimensions)
+        self.valueWeights = np.random.rand(self.num_heads, self.input_dimensions)
+
+    def attentionCalculation(self, input):
+        query = self.queryWeights.dot(input)
+        key = self.keyWeights.dot(input)
+        value = self.valueWeights.dot(input)
+
+        attention_input = np.dot(query, key.T) / np.sqrt(self.input_dimensions)
+        final_attentionValue = F.softmax(attention_input, dim=-1).dot(value)
+
+        return final_attentionValue
 
 class Encoder:
-    def __init__(self):
+    def __init__(self, num_heads, final_input, input_dimensions):
+        self.num_heads = num_heads
+        self.final_input = final_input
+        self.input_dimensions = input_dimensions
+
+    def forwardEncoding(self, input):
+        attention_heads = [AttentionHead(self.input_dimensions) for _ in range(self.num_heads)]
+        head_outputs = [head.attentionCalculation(input) for head in attention_heads]
+        concatenated_heads = np.concatenate(head_outputs, axis=-1)
+
+        residual_connections = concatenated_heads + input
+
+        feedForward = nn.ReLU(residual_connections)
+        return feedForward
 
 class Decoder:
     def __init__(self):
 
-class TransformerBlock:
-    def __init__(self, nn.module):
+class TransformerBlock(nn.module):
+    def __init__(self, num_layers, num_heads):
+        super(AttentionHead, self).__init__()
+
+        
+        #layers = [Encoder(self. )]
+
 
 class PredictionModel:
     def __init__(self, transformermodel, model_path="",):
 
-    def predict():
+    def predict(self, seq_dataPoint: DataPoint) -> np.ndarray | None:
+
+        if not seq_dataPoint.need_prediction:
+            return None
+        
         self.current_seq_ix = None
         self.sequence_history = []
         
@@ -56,3 +85,6 @@ class PredictionModel:
 
         # Initialize hidden state
         self.hidden = None
+
+        prediction = np.zeros(2) # Placeholder for prediction output
+        return prediction
